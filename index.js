f5934a65b763ee44f5c3ba15dbcb4020f4bcb0de
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-10-29 03:48:03 +0600

    connected with mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2wssq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("london-travel");
+        const hotels = database.collection("hotels");
+
+        app.get('/hotels', async (req, res) => {
+            const result = await hotels.find({}).toArray()
+            res.json(result);
+        })
+
+
+    } finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send("Travel london site is running");
+})
+
+app.listen(port, () => {
+    console.log("Running port is", port);
+})
