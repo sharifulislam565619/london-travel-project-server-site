3ff007b76c61fed4680add7d3843547503528561
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-10-29 05:00:58 +0600

    get single data implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const app = express();
 const cors = require("cors");
@@ -7,6 +8,8 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json())
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2wssq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -18,9 +21,20 @@ async function run() {
         const database = client.db("london-travel");
         const hotels = database.collection("hotels");
 
+        // get all data
         app.get('/hotels', async (req, res) => {
             const result = await hotels.find({}).toArray()
             res.json(result);
+        })
+
+        // get single data
+        app.get('/hotels/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await hotels.findOne(query);
+            res.json(result);
+
+
         })
 
 
